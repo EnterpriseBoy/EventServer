5915bf07e4c75527af7ae461cdc1e8d0527b2702
Niall Maguire <niall.maguire@topmail.ie>
2020-09-07 21:53:47 +0100

    index started

---
+++ Repos/eventsRepo.js
@@ -0,0 +1,30 @@
+let fs = require('fs');
+
+const FILE_NAME = './assets/events.json';
+
+let eventRepo = {
+     get: function(resolve,reject){
+        fs.readFile(FILE_NAME, function(err,data){
+            if(err){
+                reject(err);
+            }
+            else{
+                resolve(JSON.parse(data));
+            }
+            
+        })
+    },
+    getById: function(id,resolve,reject){
+        fs.readFile(FILE_NAME, function(err,data){
+            if(err){
+                reject(err);
+            }
+            else{
+                resolve(JSON.parse(data).find(x => x.id == id));
+            }
+            
+        })
+    }
+};
+
+module.exports = eventRepo;
--- index.js
+++ index.js
@@ -1,14 +1,56 @@
+let eventRepo = require('./Repos/eventsRepo')
 let express  = require('express');
+const { request } = require('express');
+
 let app = express();
 
 let router = express.Router();
 
+//let food= eventRepo.get();
+
+//course from Mosh https://www.youtube.com/watch?v=pKd0Rpw7O48
+
 router.get('/', function (req,res,next){
-    res.send("Event");
+    eventRepo.get(data => {
+        res.status(200).json({
+            "status":200,
+            "statusText":"OK",
+            "message":"All food returned.",
+            "data":data
+        });
+    }, err => {
+        next(err);
+    });
+});
+
+router.get('/:id',function(req,res,next){
+    eventRepo.getById(req.params.id,function(data){
+        if(data){
+            res.status(200).json({
+                "status":200,
+                "statusText":"OK",
+                "message":"Single Event Retrived",
+                "data":data
+            });
+        }
+        else{
+            res.status(404).json({
+                "status":400,
+                "statusText":"Not Found",
+                "message":"The Event with '"+req.params.id+" 'could not be found",
+                "error":{
+                    "code":"not found",
+                    "message":"The Event with '"+req.params.id+" 'could not be found"
+                }
+            });
+        };
+    });
 });
 
 app.use('/api/',router);
 
-var server = app.listen(5050, function() {
-    console.log('Node server is running on http://localhost:5050..');
+//PORT
+const PORT  = process.env.port || 5050; 
+var server = app.listen(PORT, function() {
+    console.log(`Node server is running on http://localhost:${PORT}..`);
 });
